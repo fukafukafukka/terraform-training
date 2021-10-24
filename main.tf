b98c96fc580f6570339dcddbaaf98b0e05910654
fukasawa <keisuke.fukasawa@ever-rise.co.jp>
2021-10-24 14:49:33 +0900

    http_serverのモジュール化

---
+++ main.tf
@@ -0,0 +1,18 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "region" {}
+
+provider "aws" {
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
+    region = "${var.region}"
+}
+
+module "web_server" {
+  source        = "./http_server"
+  instance_type = "t3.micro"
+}
+
+output "public_dns" {
+  value = module.web_server.public_dns
+}
